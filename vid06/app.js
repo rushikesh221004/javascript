101014fcb8602d174f01d25f430f7001550a28e1
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-12-12 19:34:45 +0530

    Datatype coversion confusion

--- vid06/app.js
+++ vid06/app.js
@@ -14,8 +14,28 @@ let valueInNumber = Number(score); // convert string and all of these value to n
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0=> false
 // "" =? false
 // "hitesh" => true
+
+let username = "rishi";
+let age = 20;
+
+let convertIntoNumber = Number(username);
+let convertIntoString = String(age);
+let convertIntoBoolean = Boolean(age);
+let convertIntoObject = Object(age);
+
+console.log(typeof convertIntoNumber); // number
+console.log(convertIntoNumber); // NaN
+
+console.log(typeof convertIntoString); // string
+console.log(convertIntoString); // 20
+
+console.log(typeof convertIntoBoolean); // boolean
+console.log(convertIntoBoolean); // true
+
+console.log(typeof convertIntoObject); // object
+console.log(convertIntoObject); // [Number: 20]

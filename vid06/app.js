0e77b8225346574ed91496e14c4b492aa60d1b3e
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-12-12 19:21:14 +0530

    Datatype conversion confusion

---
+++ vid06/app.js
@@ -0,0 +1,21 @@
+let score = null;
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score); // convert string and all of these value to number
+//console.log(typeof valueInNumber); // number
+//console.log(valueInNumber); // NaN - Not a Number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" =? false
+// "hitesh" => true

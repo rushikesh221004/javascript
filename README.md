7ef41afc0e0bc02aac507d0b70574beead17cbce
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-12-12 19:07:14 +0530

    Datatypes and ECMA standards

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javascript
+
+1. vid05 - Datatypes and ECMA standards
---
+++ vid05/app.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+let name = "rishi"; // string datatype
+let age = 18; // number datatype
+let isLoggedIn = false; // boolean datatype
+
+// Datatyptes 👇
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => matlab value assign nhi hui hai
+// symbol => jab bhi hame baat karni hai unique ke liye tab ham symbol ke baare mai baat karte hai
+
+// object
+
+console.log(typeof {}); // object datatype
+console.log(typeof null); // object datatype
+console.log(typeof undefined); // undefined datatype
